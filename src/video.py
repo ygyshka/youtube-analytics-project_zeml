b22ca0eae5f49fa52cecff9f63bb433e64817313
WHAT <ygyshka0@gmail.com>
2023-07-14 19:26:13 +0700

    add try/exception

--- src/video.py
+++ src/video.py
@@ -7,17 +7,27 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
+
         self.video_id = video_id
-        self.statistic = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                     id=video_id
-                                                     ).execute()
-        self.name = self.statistic['items'][0]['snippet']['title']
-        self.link = "https://www.youtube.com/channel/" + video_id
-        self.view_count = self.statistic['items'][0]['statistics']['viewCount']
-        self.view_like = self.statistic['items'][0]['statistics']['likeCount']
+        self.title = None
+        self.like_count = None
+
+        try:
+            self.video_id = video_id
+
+            self.statistic = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=video_id
+                                                         ).execute()
+            self.title = self.statistic['items'][0]['snippet']['title']
+            self.link = "https://www.youtube.com/channel/" + video_id
+            self.view_count = self.statistic['items'][0]['statistics']['viewCount']
+            self.like_count = self.statistic['items'][0]['statistics']['likeCount']
+        except Exception as ex:
+
+            print("Пользователь передал несуществующий id видео!")
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
 
 class PLVideo(Video):
